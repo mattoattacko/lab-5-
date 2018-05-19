ff9b9705f58fa335bae7217f48d59dbaa8a478eb
Matt McQuain <matt.mcquain@gmail.com>
2018-05-19 15:12:12 -0700

    no really maybe this time is correct?

--- app.js
+++ app.js
@@ -93,14 +93,14 @@ var firstPart = sumAndMultiply(testArray[0], testArray[1], testArray[2]);
 
 var mySum = firstPart[0]
 
-var message3 = (testArray[0] + ',' + testArray[1] + ',' + testArray[2] + 'was passed in as an array of numbers, and ' + firstPart[0] + ' is their sum.')
+var message3 = (testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + firstPart[0] + ' is their sum.')
 
 return [mySum, message3]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
